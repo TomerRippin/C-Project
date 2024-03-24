af31fba6edf3bd99c049d5b1332073e7c33409ec
trippin <totomir12@gmail.com>
2024-03-24 23:33:03 +0200

    remove found from searchList

--- Header Files/linked_list.h
+++ Header Files/linked_list.h
@@ -1,4 +1,4 @@
-// Define a structure for the linked list node
+/* Define a structure for the linked list node */ 
 typedef struct ListNode {
     char *name; // Name associated with the Node
     char *data; // The data stored in the Node  
@@ -6,23 +6,22 @@ typedef struct ListNode {
     struct ListNode *next; // Pointer to the next node
 } ListNode;
 
-// Define a structure for the linked list
+/* Define a structure for the linked list */ 
 typedef struct {
     ListNode *head;
 } LinkedList;
 
-// Function to initialize an empty linked list
-void initialize_list(LinkedList *list);
+/* Function to initialize an empty linked list */
+void initializeList(LinkedList *list);
 
-// Function to insert data at the beginning of the list
-void insert_to_list(LinkedList *list, char *name, char *data, int line_number);
+/* Function to insert data at the beginning of the list */ 
+void insertToList(LinkedList *list, char *name, char *data, int line_number);
 
-// Function to search for data in the list
-ListNode *search_list(const LinkedList *list, const char *target_name, int *found);
+/* Function to search for data in the list */ 
+ListNode *searchList(const LinkedList *list, const char *target_name, int *found);
 
-// Function to free memory allocated for the list
-void free_list(LinkedList *list, void (*free_data)(void *));
+/* Function to free memory allocated for the list */
+void freeList(LinkedList *list);
 
-// Function to free memory from a node
-void free_node(ListNode *node);
-
+/* Function to free memory from a node */
+void freeNode(ListNode *node);
