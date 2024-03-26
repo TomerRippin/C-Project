d033051358d660489596cbe7ce3a02b94f26a98c
trippin <totomir12@gmail.com>
2024-03-27 00:39:29 +0200

    more elaborated documentation

--- Header Files/utils.h
+++ Header Files/utils.h
@@ -1,10 +1,31 @@
 # include <stdio.h>
 
-/* Function to remove extra spaces and tabs from a string */
+/**
+ * @brief Removes extra spaces and tabs from a string.
+ *
+ * This function removes extra spaces and tabs from the given string.
+ * Additionally, it ensures that only one space remains if spaces are found at the beginning of the string.
+ *
+ * @param str The string to be modified (in-place).
+ */
 void removeExtraSpacesTabs(char *str);
 
-/* Function to remove comments (lines starting with ';') from a string */
+/**
+ * @brief Removes comments (lines starting with ';') from a string.
+ *
+ * This function removes comments (lines starting with ';') from the given string.
+ *
+ * @param str The string to be modified (in-place).
+ */
 void removeComments(char *str);
 
-/* Function to remove extra spaces, tabs, and comments from a file and create a modified file */
+/**
+ * @brief Removes extra spaces, tabs, and comments from a file.
+ *
+ * This function reads an input file line by line, removes extra spaces, tabs, and comments from each line,
+ * and then writes the modified lines to an output file.
+ *
+ * @param inputFile A pointer to the input file.
+ * @param outputFilename A pointer to the output file.
+ */
 int removeExtraSpacesTabsAndComments(FILE *inputFile, FILE *outputFile);
