c7b79b0c2001e09b489acc5ee204dc9a79ebe1f9
Lilach Shnaidman <lilach@shnaidman.com>
2024-03-29 21:09:15 +0300

    added opcodes and instructions to constants

--- Header Files/constants.h
+++ Header Files/constants.h
@@ -5,5 +5,30 @@
 
 #define DEFINE_DIRECTIVE ".define"
 #define NUM_INSTRUCTIONS 4
+#define NUM_OPCODES 15
 
 char *INSTRUCTIONS[] = {".data", ".string", ".extern", ".entry"};
+
+typedef struct Opcode
+{
+    char *name;
+    int operandsNum;
+} Opcode;
+
+Opcode OPCODES[] = {
+    {"mov", 2},
+    {"cmp", 2},
+    {"add", 2},
+    {"sub", 2},
+    {"lea", 2},
+    {"not", 1},
+    {"clr", 1},
+    {"inc", 1},
+    {"dec", 1},
+    {"jmp", 1},
+    {"bne", 1},
+    {"red", 1},
+    {"prn", 1},
+    {"jsr", 1},
+    {"rts", 0},
+    {"hlt", 0}};
