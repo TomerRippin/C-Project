647858ae060241eb974dffb502c1ba4cbb07290d
trippin <totomir12@gmail.com>
2024-04-05 17:05:21 +0300

    move operands prsing to a new file

---
+++ Header Files/operands_parser.h
@@ -0,0 +1,11 @@
+#ifndef OPERANDS_PARSER_H
+#define OPERANDS_PARSER_H
+
+/**
+ * Function to parse the type of operand.
+ * @param operand The input operand string to be parsed.
+ * @return The type of operand.
+ */
+int parseOperandAdressing(const char *operand);
+
+#endif /* OPERANDS_PARSER_H */
--- Source Files/operands_parser.c
+++ Source Files/operands_parser.c
@@ -176,3 +176,63 @@ void freeOperand(Operand *operand){
     free(operand->type);
     free(operand->value);
 }
+
+
+int parseOperandAdressing(const char *operand)
+{
+    char *ch;
+    /* Check if the operand is empty
+    TODO: return an Error Code  */ 
+    if (*operand == '\0') {
+        return -1;
+    }
+
+    /* Check for immediate addressing */ 
+    if (*operand == '#') {
+        /* Check if the remaining characters are digits */ 
+        while (*(++operand) != '\0') {
+            if (!isdigit(*operand)) {
+                return -1; /* Invalid immediate address TODO: return an Error Code */ 
+            }
+        }
+        return 0;
+    }
+
+    /* Check for register addressing */ 
+    if (operand[0] == 'r' && isdigit(operand[1]) && operand[2] == '\0') {
+        return 3;
+    }
+
+    /* Check for straight addressing or index addressing */ 
+    const char *labelEnd = strchr(operand, '[');
+    if (labelEnd != NULL) {
+        /* Check if the label is followed by '[' and ']' */ 
+        const char *indexStart = labelEnd + 1;
+        const char *indexEnd = strchr(indexStart, ']');
+        if (indexEnd != NULL && labelEnd - operand > 0 && indexEnd - indexStart > 0) {
+            /* Check if the label contains only valid characters */ 
+            for (*ch = operand; ch < labelEnd; ch++) {
+                if (!isalnum(*ch) && *ch != '_') {
+                    return -1; /* Invalid label  TODO: return an Error Code*/ 
+                }
+            }
+            /* Check if the index contains only digits */ 
+            for (*ch = indexStart; ch < indexEnd; ch++) {
+                if (!isdigit(*ch)) {
+                    return -1; /* Invalid index  TODO: return an Error Code */ 
+                }
+            }
+            return 2;
+        }
+    } else {
+        /* Check if the operand contains only valid characters for a label */ 
+        for (*ch = operand; *ch != '\0'; ch++) {
+            if (!isalnum(*ch) && *ch != '_') {
+                return -1; /* Invalid label  TODO: return an Error Code */ 
+            }
+        }
+        return 1; /* Straight adressing */
+    }
+
+    return -1; /* No valid addressing type found  TODO: return an Error Code */ 
+}
