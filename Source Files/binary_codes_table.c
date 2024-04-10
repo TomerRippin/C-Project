8ae37b9552481016cfc4db0bfcdada04889281a0
ls <you@example.com>
2024-04-10 15:22:15 +0300

    created new data structures - symbolTable and a BinaryCodesTable

---
+++ Source Files/binary_codes_table.c
@@ -0,0 +1,63 @@
+#include "binary_codes_table.h"
+
+/* TODO: move to a strctures folder */
+
+BinaryCodesTable *createBinaryCodesTable()
+{
+    BinaryCodesTable *table = (BinaryCodesTable *)malloc(sizeof(BinaryCodesTable));
+    if (table == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    table->head = NULL;
+    table->last = NULL;
+    table->length = 0;
+    return table;
+}
+
+void insertToBinaryCodesTable(BinaryCodesTable *table, int decAddress, char *sourceLine, char *binaryCode)
+{
+    BinaryCodesNode *newNode = (BinaryCodesNode *)malloc(sizeof(BinaryCodesNode));
+    /* TODO: is it really needed? maybe were Magzimim */
+    if (newNode == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    newNode->decAddress = decAddress;
+    strcpy(newNode->sourceLine, sourceLine);
+    strcpy(newNode->binaryCode, binaryCode);
+    newNode->next = NULL;
+
+    if (table->head == NULL) {
+        table->head = newNode;
+    }
+    if (table->last != NULL) {
+        table->last->next = newNode;
+    }
+    table->last = newNode;
+    table->length++;
+}
+
+void freeBinaryCodesNode(BinaryCodesNode *node)
+{
+    free(node->sourceLine);
+    free(node->binaryCode);
+    free(node);
+}
+
+/* Function to free memory allocated for the list */
+void freeBinaryCodesTable(BinaryCodesTable *table) {
+    BinaryCodesNode *current = table->head;
+    BinaryCodesNode *tempNodeToFree;
+    while (current != NULL)
+    {
+        tempNodeToFree = current;
+        current = current->next;
+        freeNode(tempNodeToFree);
+    }
+    table->head = NULL;
+    table->last = NULL;
+    free(table);
+}
